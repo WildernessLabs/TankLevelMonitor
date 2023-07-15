738d992b0c52f67b0c695584f72dcadfc3233fc3
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-07-14 18:14:07 -0700

    Add TankLevelMonitor_UI

---
+++ Source/TankLevelMonitor_UI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Meadow;
+using Meadow.Foundation.Displays;
+
+namespace TankLevelMonitor_UI
+{
+    public class MeadowApp : App<Windows>
+    {
+        private WinFormsDisplay _display = default!;
+        private DisplayController _displayController;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Creating Outputs");
+
+            _display = new WinFormsDisplay(width: 480, height: 320);
+
+            _displayController = new DisplayController(_display);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            _displayController.Update();
+
+            Application.Run(_display);
+
+            return Task.CompletedTask;
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
+
+            await MeadowOS.Start(args);
+        }
+    }
+}
