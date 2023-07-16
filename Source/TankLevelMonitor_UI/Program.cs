f44b1d8ed2785d45b5dc09d2749debaeda0a3da3
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-07-16 01:16:01 -0700

    Simulate tank level and atmospheric value for WinForms UI sample

--- Source/TankLevelMonitor_UI/Program.cs
+++ Source/TankLevelMonitor_UI/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Meadow;
 using Meadow.Foundation.Displays;
+using Meadow.Units;
+using System.Net.Http.Headers;
 
 namespace TankLevelMonitor_UI
 {
     public class MeadowApp : App<Windows>
     {
         private WinFormsDisplay _display = default!;
-        private DisplayControllerWithStatus _displayController;
+        private DisplayController _displayController;
+        //private DisplayControllerWithStatus _displayController;
 
         public override Task Initialize()
         {
@@ -14,7 +17,8 @@ namespace TankLevelMonitor_UI
 
             _display = new WinFormsDisplay(width: 320, height: 240);
 
-            _displayController = new DisplayControllerWithStatus(_display);
+            _displayController = new DisplayController(_display);
+            //_displayController = new DisplayControllerWithStatus(_display);
 
             return Task.CompletedTask;
         }
@@ -24,11 +28,48 @@ namespace TankLevelMonitor_UI
             Console.WriteLine("Run...");
 
             //_displayController.ShowSplashScreen();
-            _displayController.VolumePercent = 95;
+
+            Task.Run(() => 
+            {
+                while (true)
+                {
+                    _displayController.AtmosphericConditions = RandomAtmosphericValue();
+
+                    for (int i = 0; i <= 100; i++)
+                    {
+                        _displayController.VolumePercent = i;
+                        Thread.Sleep(100);
+                    }
+
+                    _displayController.AtmosphericConditions = RandomAtmosphericValue();
+
+                    for (int i = 100; i >= 0; i--)
+                    {
+                        _displayController.VolumePercent = i;
+                        Thread.Sleep(100);
+                    }
+                }
+            });
 
             Application.Run(_display);
 
             return Task.CompletedTask;
+        }
+
+        private (Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure, Resistance? GasResistance)? RandomAtmosphericValue() 
+        {
+            var random = new Random();
+
+            Temperature? temperature = new Temperature?(new Temperature(random.Next(24, 29)));
+            RelativeHumidity? Humidity = new RelativeHumidity?(new RelativeHumidity(random.Next(75, 85)));
+            Pressure? pressure = new Pressure(new Pressure(random.Next(1100, 1200), Pressure.UnitType.Millibar));
+            Resistance? resistance = new Resistance(new Resistance(55));
+
+            var tuple = (
+                temperature, Humidity, pressure, resistance
+            );
+
+            return tuple;
         }
 
         public static async Task Main(string[] args)
