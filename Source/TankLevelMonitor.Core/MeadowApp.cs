9081e52fe930f4beb48ec16182daa0bd99d68eaa
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-07-15 00:56:59 -0700

    Turning app to StartKit

---
+++ Source/TankLevelMonitor.Core/MeadowApp.cs
@@ -0,0 +1,25 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System.Threading.Tasks;
+
+namespace TankLevelMonitor.Core
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Resolver.Log.Info("Run...");
+
+            Resolver.Log.Info("Hello, Meadow Core-Compute!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Source/TankLevelMonitor.Desktop/Program.cs
@@ -0,0 +1,13 @@
+﻿using Meadow;
+using System.Threading.Tasks;
+
+namespace TankLevelMonitor.DesktopApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await MeadowOS.Start(args);
+        }
+    }
+}
